e6bbbb2abb0699f6e28d14cd32d0a760339d7708
ZXGuo-code <zhengxuguo1999@gmail.com>
2024-07-02 16:48:54 +0800

    Update pymtrd_example.py

--- pymtrd_example.py
+++ pymtrd_example.py
@@ -4,7 +4,7 @@ import pymtrd_process as pp
 
 if __name__ == '__main__':
     
-    df = pd.read_csv('input_new.csv', header=None)
+    df = pd.read_csv('input.csv', header=None)
     pp.process(df)
     # df = pd.read_csv('input_draw.csv', header=None)
     # pp.process_draw(df)
